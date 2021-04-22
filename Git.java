1ff8f485128fc40cf115a7ba7f76d5dd5e9941dd
Jostin <jostinparodi@gmail.com>
2021-04-22 22:03:11 +0200

    Comienzo del Proyecto

---
+++ Git.java
@@ -0,0 +1,17 @@
+package git;
+
+
+import java.util.*;
+
+public class Git{
+
+
+    public static void main(String[] args) {
+        String Prueba;
+        Scanner scnLeer = new Scanner(System.in);
+
+        System.out.println("Esta es una prueba de git");
+
+    }
+
+}
